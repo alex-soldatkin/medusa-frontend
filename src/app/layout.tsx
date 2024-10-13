ce3b604d40f7707a6bc224577a845fd3f69cc468
alex-soldatkin <alex.soldatkin@me.com>
2024-10-13 14:39:53 +0100

    add pwa config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,11 @@ export const metadata: Metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="ru" data-mode="light">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+      </head>
       <body>
         <main className="relative">{props.children}</main>
       </body>
